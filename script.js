f52d82a6efa6c7db0e0ae7381d474c0ee16ad2c9
fabuleux366 <abbakikyungu@gmail.com>
2023-03-20 22:13:20 -0400

    configuration de eslint

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-const brightNess = document.querySelector(".uil-brightness-low");
-const body = document.querySelector("body");
+const selectors = {
+  brightnessButton: ".uil-brightness-low",
+  body: "body",
+  toggleButton: ".uil-bars",
+  navHeader: ".nav--header",
+  logo: ".imageLogo",
+  forumButton: ".forum",
+  blogButton: ".blog",
+  searchButton: ".uil-search",
+  closeButton: ".uil-times",
+  leftNav: ".ulNavContainerLeft",
+  rightNav: ".ulNavContainerRight",
+};
 
-brightNess.addEventListener("click", function () {
-  body.style.backgroundColor = "#d53c42";
-  navHeader.style.backgroundColor = "#d53c42";
+const elements = {};
+for (const key in selectors) {
+  // eslint-disable-next-line no-undef
+  elements[key] = document.querySelector(selectors[key]);
+}
+
+elements.brightnessButton.addEventListener("click", function () {
+  elements.body.style.backgroundColor = "#d53c42";
+  elements.navHeader.style.backgroundColor = "#d53c42";
 });
 
-const navLeft = document.querySelector(".ulNavContainerLeft");
-const togle = document.querySelector(".uil-bars");
-const navHeader = document.querySelector(".nav--header");
-const imgLogo = document.querySelector(".imageLogo");
-const header = document.querySelector("header");
-const forum = document.querySelector(".forum");
-const blog = document.querySelector(".blog");
-const search = document.querySelector(".uil-search");
-const times = document.querySelector(".uil-times");
-const navRight = document.querySelector(".ulNavContainerRight");
-const rli = document.querySelector(".rli");
-togle.addEventListener("click", function () {
-  navLeft.style.flexDirection = "column";
-
-  navLeft.style.borderRadius = "5px";
-  navHeader.style.height = "300px";
-  navHeader.style.borderBottom = "none";
-  navLeft.style.marginTop = "120px";
-  navLeft.style.marginLeft = "120px";
-  imgLogo.style.visibility = "hidden ";
-  forum.style.visibility = "visible";
-  forum.style.display = "flex";
-  blog.style.visibility = "visible";
-  blog.style.display = "flex";
-  search.style.display = "none";
-  togle.style.visibility = "hidden";
-  togle.style.display = "none";
-  times.style.display = "flex";
-  navRight.style.padding = "30px 0px 0 40px";
+elements.toggleButton.addEventListener("click", function () {
+  elements.leftNav.style.flexDirection = "column";
+  elements.leftNav.style.borderRadius = "5px";
+  elements.navHeader.style.height = "300px";
+  elements.navHeader.style.borderBottom = "none";
+  elements.leftNav.style.margin = "120px 0 0 120px";
+  elements.logo.style.visibility = "hidden";
+  elements.forumButton.style.visibility = "visible";
+  elements.forumButton.style.display = "flex";
+  elements.blogButton.style.visibility = "visible";
+  elements.blogButton.style.display = "flex";
+  elements.searchButton.style.display = "none";
+  elements.toggleButton.style.display = "none";
+  elements.closeButton.style.display = "flex";
+  elements.rightNav.style.padding = "30px 0 0 40px";
 });
 
-times.addEventListener("click", function () {
-  navLeft.style.flexDirection = "row";
-  navLeft.style.borderRadius = "0";
-  navHeader.style.height = "80px";
-  navHeader.style.borderBottom = "1px solid gray";
-  navLeft.style.margin = "0 auto";
-  imgLogo.style.visibility = "visible ";
-  forum.style.display = "none";
-  blog.style.display = "none";
-  search.style.display = "flex";
-  togle.style.visibility = "visible";
-  togle.style.display = "flex";
-  times.style.display = "none";
-  navRight.style.padding = "30px 10px 0 60px";
-
+elements.closeButton.addEventListener("click", function () {
+  elements.leftNav.style.flexDirection = "row";
+  elements.leftNav.style.borderRadius = "0";
+  elements.navHeader.style.height = "80px";
+  elements.navHeader.style.borderBottom = "1px solid gray";
+  elements.leftNav.style.margin = "0 auto";
+  elements.logo.style.visibility = "visible";
+  elements.forumButton.style.display = "none";
+  elements.blogButton.style.display = "none";
+  elements.searchButton.style.display = "flex";
+  elements.toggleButton.style.display = "flex";
+  elements.closeButton.style.display = "none";
+  elements.rightNav.style.padding = "30px 10px 0 60px";
 });
